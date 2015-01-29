95a05a2eb7ddaa4c6dd5cdb6df6abb144bab82d1
Yissakhar Z. Beck <yissakhar.beck@manheim.com>
2015-01-28 21:45:28 -0500

    Added preliminary resize code.

--- imgsize.go
+++ imgsize.go
@@ -2,15 +2,71 @@ package main
 
 import (
 	"fmt"
+	"github.com/nfnt/resize"
 	"net/http"
 	"os"
+	"strconv"
+
+	"image"
+	_ "image/gif"
+	_ "image/jpeg"
+	"image/png"
 )
+
+var methods = map[string]resize.InterpolationFunction{
+	"nearest-neighbor":  resize.NearestNeighbor,
+	"bilinear":          resize.Bilinear,
+	"bicubic":           resize.Bicubic,
+	"MitchellNetravali": resize.MitchellNetravali,
+	"lanczos2":          resize.Lanczos2,
+	"lanczos3":          resize.Lanczos3,
+}
+
+func handleResize(rw http.ResponseWriter, req *http.Request) {
+	q := req.URL.Query()
+	url := q.Get("url")
+	width, _ := strconv.ParseInt(q.Get("width"), 10, 0)
+	height, _ := strconv.ParseInt(q.Get("height"), 10, 0)
+	method := q.Get("method")
+
+	if (width < 0) || (height < 0) {
+		panic("Width and height must be positive.")
+	}
+
+	if _, ok := methods[method]; !ok {
+		panic("Unknown method: " + method)
+	}
+
+	rsp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer rsp.Body.Close()
+
+	img, _, err := image.Decode(rsp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	img = resize.Resize(uint(width), uint(height), img, methods[method])
+	if err != nil {
+		panic(err)
+	}
+
+	rw.Header().Set("Content-Type", "image/png")
+
+	err = png.Encode(rw, img)
+	if err != nil {
+		panic(err)
+	}
+}
 
 func handleMain(rw http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(rw, "This is a test.")
 }
 
 func main() {
+	http.HandleFunc("/resize", handleResize)
 	http.HandleFunc("/", handleMain)
 
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
